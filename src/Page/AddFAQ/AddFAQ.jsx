f28b2971f45303f54f81e1194b390c146c604fac
Showrove Roy <showrove200@gmail.com>
2023-12-22 21:16:43 +0600

    Add faq done

--- src/Page/AddFAQ/AddFAQ.jsx
+++ src/Page/AddFAQ/AddFAQ.jsx
@@ -1,3 +1,40 @@
 export const AddFAQ = () => {
-  return <div>AddFAQ</div>;
+  return (
+    <div className='FormCardBG'>
+      <h5 className='fromTitle'>
+        Add FAQ
+      </h5>
+
+      <div className=''>
+        <form>
+          {/* Question */}
+          <label className='form-control w-full'>
+            <div className='label'>
+              <span className='label-text text-lg font-medium'>Question</span>
+            </div>
+            <input
+              type='text'
+              placeholder='Enter Your Question'
+              className='input w-full formInputBox focus:outline-none focus:border-blue'
+            />
+          </label>
+
+          {/* Message box */}
+          <label className='form-control mt-5'>
+            <div className='label'>
+              <span className='label-text text-lg font-medium'>Answer</span>
+            </div>
+            <textarea
+              className='textarea min-h-20 formInputBox focus:outline-none focus:border-blue'
+              placeholder='Enter Your Answer'></textarea>
+          </label>
+
+          <div className='mt-5 flex gap-5'>
+            <button className='btnFill'>SAVE</button>
+            <button className='btnOutline'>CANCEL</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
