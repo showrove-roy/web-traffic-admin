640e0b4dbc90c2490f4e9d67ef577ac28ed5a97a
Showrove Roy <showrove200@gmail.com>
2024-01-02 02:36:20 +0600

    Load more btn delete

--- src/Components/BlogSection/BlogSection.jsx
+++ src/Components/BlogSection/BlogSection.jsx
@@ -1,15 +1,15 @@
 /* eslint-disable react/prop-types */
-
+import { useState } from "react";
 import { FeaturedBlogCard } from "../FeaturedBlogCard/FeaturedBlogCard";
-import { OutlineBtn } from "../OutlineBtn/OutlineBtn";
 import { TitleSection } from "../TitleSection/TitleSection";
 
 export const BlogSection = ({ id, CatagoryBlogs, refetch, isLoading }) => {
+  const [numOfData, setNumOfData] = useState(5);
   return (
     <div className='CMNCardBG'>
       <TitleSection title={"Featured Blogs"} link={`add-featured-blog/${id}`} />
 
-      {CatagoryBlogs.map((blog) => (
+      {CatagoryBlogs?.slice(0, numOfData)?.map((blog) => (
         <FeaturedBlogCard
           key={blog.id}
           blog={blog}
@@ -18,9 +18,15 @@ export const BlogSection = ({ id, CatagoryBlogs, refetch, isLoading }) => {
         />
       ))}
 
-      <div className='mt-5 flex justify-center'>
-        <OutlineBtn btnLink={"hello"} btnText={"Load More"} />
-      </div>
+      {CatagoryBlogs.length >= numOfData && (
+        <div className='flex justify-center mb-20 mt-20'>
+          <button
+            onClick={() => setNumOfData(numOfData + 5)}
+            className='py-3 px-8 text-blue border-2 font-medium border-blue rounded-full hover:btnShadow w-fit text-sm'>
+            Load More
+          </button>
+        </div>
+      )}
     </div>
   );
 };
--- src/Components/SubServices/SubServices.jsx
+++ src/Components/SubServices/SubServices.jsx
@@ -1,19 +1,18 @@
 /* eslint-disable react/prop-types */
-import { OutlineBtn } from "../OutlineBtn/OutlineBtn";
 import { SubServiceTable } from "../SubServiceTable/SubServiceTable";
 import { TitleSection } from "../TitleSection/TitleSection";
 
-export const SubServices = ({ id, subService, refetch ,isLoading}) => {
+export const SubServices = ({ id, subService, refetch, isLoading }) => {
   return (
     <div className='CMNCardBG'>
       <TitleSection title={"Sub Service"} link={`add-sub-service/${id}`} />
 
       {/* data Table */}
-      <SubServiceTable subService={subService} refetch={refetch} isLoading={isLoading} />
-
-      <div className='mt-5 flex justify-center'>
-        <OutlineBtn btnLink={"hello"} btnText={"Load More"} />
-      </div>
+      <SubServiceTable
+        subService={subService}
+        refetch={refetch}
+        isLoading={isLoading}
+      />
     </div>
   );
 };
