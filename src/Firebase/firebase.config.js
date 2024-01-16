0b847b87c90977a217c6e7e6103bfaff3efcbe88
unknown <mojahidislammishat@gmail.com>
2024-01-16 06:25:00 +0600

    as

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,18 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDoc3v5i8Ecg55yXiK7Fhrewp8pPnm_O0M",
-  authDomain: "web-traffic-769e9.firebaseapp.com",
-  projectId: "web-traffic-769e9",
-  storageBucket: "web-traffic-769e9.appspot.com",
-  messagingSenderId: "362630959161",
-  appId: "1:362630959161:web:a543550752135115abfde8"
+  apiKey: "AIzaSyB13CYEefNandrVYQKSuiRJYLdW-7PUOiQ",
+  authDomain: "web-traffic-777bf.firebaseapp.com",
+  projectId: "web-traffic-777bf",
+  storageBucket: "web-traffic-777bf.appspot.com",
+  messagingSenderId: "775203195842",
+  appId: "1:775203195842:web:0702e7e8d8ced0b8ba245b",
+  measurementId: "G-3E7WDC7GFN",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export default app;
